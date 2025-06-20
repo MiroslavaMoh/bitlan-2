8261b9a14b081abd3e93eac3d2246bf0076fbe99
MiroslavaMoh <miroslavamoh252@gmail.com>
2025-06-20 10:39:36 -0600

    Reorganizar-carpetas

--- android/app/src/main/kotlin/com/example/fintech_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.fintech_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- android/app/src/main/kotlin/com/example/fintech_ui_tutorial/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.fintech_ui_tutorial
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
